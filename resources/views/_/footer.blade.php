b8c6bf8d4020b808aceb44feb17f5799b44c9929
Freddy Egnin <116601041+black-screen07@users.noreply.github.com>
2022-10-25 05:39:12 +0200

    Update footer.blade.php

--- resources/views/_/footer.blade.php
+++ resources/views/_/footer.blade.php
@@ -3,7 +3,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <a href="https://www.facebook.com/profile.php?id=100013474415094" target="_blank">Egnin Aka Emmanuel Freddy</a> </span>
+            <span>Copyright &copy; <a href="https://www.linkedin.com/in/aka-freddy-emmanuel-egnin-642805195/" target="_blank">Egnin Aka Emmanuel Freddy</a> </span>
           </div>
         </div>
       </footer>
